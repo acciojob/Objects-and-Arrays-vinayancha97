8a9fea71ede091355dbe82e058e04f03af2da437
vinayancha97 <135926111+vinayancha97@users.noreply.github.com>
2023-07-30 09:17:51 +0530

    created a submission

--- script.js
+++ script.js
@@ -7,7 +7,7 @@ const person = {
 
 // Write your code here, make sure the name of variables is exactly same as mentioned in the problem statement
 const team = players;
-const team1 = players.splice();
+const team1 = players.slice();
 const cap1={...person};
 
 
